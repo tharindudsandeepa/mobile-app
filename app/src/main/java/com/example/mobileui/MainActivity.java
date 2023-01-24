4a972f3250bd76b869138abcf6d7545d615f285a
Tharindu Sandeepa <73959253+tharindudsandeepa@users.noreply.github.com>
2023-01-24 18:12:49 +0530

    test commit

--- app/src/main/java/com/example/mobileui/MainActivity.java
+++ app/src/main/java/com/example/mobileui/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        System.out.println("Test");
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
